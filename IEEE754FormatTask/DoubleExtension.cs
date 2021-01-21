4c2353cdc76a24db7ce5c013e7f940d447f80005
Дмитрий Шепелев <shepelev.dm@gmail.com>
2021-01-21 23:38:12 +0300

    Done

--- IEEE754FormatTask/DoubleExtension.cs
+++ IEEE754FormatTask/DoubleExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace IEEE754FormatTask
 {
@@ -12,7 +13,43 @@ namespace IEEE754FormatTask
         /// <returns>A string representation of a double type number in the IEEE754 format.</returns>
         public static string GetIEEE754Format(this double number)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            UnionStruct x = new UnionStruct
+            {
+                D = number,
+            };
+            return x.ToString();
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct UnionStruct
+        {
+            [FieldOffset(0)]
+            public double D;
+
+            [FieldOffset(0)]
+            private long l;
+
+            public new string ToString()
+            {
+                long mask = 0x01;
+                var result = new char[64];
+                for (int i = 0; i < 64; i++)
+                {
+                    if ((this.l & mask) == 0)
+                    {
+                        result[i] = '0';
+                    }
+                    else
+                    {
+                        result[i] = '1';
+                    }
+
+                    mask <<= 1;
+                }
+
+                Array.Reverse(result);
+                return new string(result);
+            }
         }
     }
 }
